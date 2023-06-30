e039da15c41b91002f5cbe95266af8f33f414858
User <user@AdmindeMacBook-Air.local>
2023-06-30 11:42:27 +0800

    feat: add new core

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+	"web-server/framework"
+)
+
+func main() {
+	server := &http.Server{
+		Handler: framework.NewCore(),
+		Addr:    "localhost:8080",
+	}
+	server.ListenAndServe()
+}
