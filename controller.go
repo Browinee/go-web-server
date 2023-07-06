8f057f123f9f6b1182783270853f592d37127c96
User <user@AdmindeMacBook-Air.local>
2023-07-06 17:28:36 +0800

    feat: add edge case check and encapulate context

---
+++ controller.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+	"web-server/framework"
+)
+
+
+func FooControllerHandler(ctx *framework.Context) error {
+	durationCtx, cancel := context.WithTimeout(ctx.BaseContext(), time.Duration(1*time.Second))
+	defer cancel()
+	panicChan := make(chan interface{}, 1)
+	finish := make(chan struct{}, 1)
+	go func() {
+		defer func() {
+			if p := recover() ; p!=nil {
+				panicChan <- p
+			}
+		}()
+		time.Sleep(10 * time.Second)
+		ctx.Json(200, "ok")
+
+		finish <- struct{}{}
+	}()
+
+	select {
+	case p := <-panicChan:
+		ctx.WriterMux().Lock()
+		defer ctx.WriterMux().Unlock()
+		log.Println(p)
+		ctx.Json(500, "panic")
+	case <-finish:
+		fmt.Println("finish")
+	case <-durationCtx.Done():
+		ctx.WriterMux().Lock()
+		defer ctx.WriterMux().Unlock()
+		ctx.Json(500, "time out")
+		ctx.SetHasTimeout()
+	}
+	return nil
+}
--- route.go
+++ route.go
@@ -1,5 +1,7 @@
 package main
 
-func routeHandler() {
+import "web-server/framework"
 
+func registerRouter(core *framework.Core) {
+	core.Get("foo", FooControllerHandler)
 }
